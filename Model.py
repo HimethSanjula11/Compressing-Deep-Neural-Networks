ddf1d3187066234ab6a9e42461239425ac5066bd
HimethSanjula11 <103920239+HimethSanjula11@users.noreply.github.com>
2025-09-09 22:24:06 +0100

    Models

---
+++ Model.py
@@ -0,0 +1,14 @@
+import torch
+import torchvision.models as models
+
+# Download pretrained ResNet-18
+resnet18 = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
+resnet18.eval()
+torch.save(resnet18.state_dict(), "resnet18.pth")
+print("ResNet-18 saved as resnet18.pth")
+
+# Download pretrained MobileNetV3-Small
+mobilenet_v3_small = models.mobilenet_v3_small(weights=models.MobileNet_V3_Small_Weights.IMAGENET1K_V1)
+mobilenet_v3_small.eval()
+torch.save(mobilenet_v3_small.state_dict(), "mobilenet_v3_small.pth")
+print("MobileNetV3-Small saved as mobilenet_v3_small.pth")
